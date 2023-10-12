097cecc7676e0ab34369d24fc2ed9ded3bc0173c
Babajide Oluwaferanmi <babajideoluwaferanmi301@gmail.com>
2023-10-12 23:30:16 +0100

    Got the logic of the Calculator to work FInally got the calculator to work, only finishing touches left now

--- script.js
+++ script.js
@@ -7,28 +7,46 @@ const numbers = document.querySelectorAll(".number");
 const clearButton = document.querySelector(".clear");
 const allClear = document.querySelector(".ac");
 const operators = document.querySelectorAll(".operator");
+const equal = document.querySelector(".equal");
 
 const add = function (a, b) {
-  a + b;
+  return a + b;
 };
 const subtract = function (a, b) {
-  a - b;
+  return a - b;
 };
 
 const multiply = function (a, b) {
-  a * b;
+  return a * b;
 };
 
 const divide = function (a, b) {
-  a / b;
+  return a / b;
 };
 
-let firstNum = 0;
-let secNum = 0;
-let operator = ``;
+function operate(num1, num2, opp) {
+  const realNum2 = num2.split("").shift();
+  const num1Digit = Number(num1);
+  const num2Digit = Number(realNum2);
+  if (opp === "+") {
+    return add(num1Digit, num2Digit);
+  } else if (opp === "-") {
+    return subtract(num1Digit, num2Digit);
+  } else if (opp === "*") {
+    return multiply(num1Digit, num2Digit);
+  } else if (opp === "/") {
+    return divide(num1Digit, num2Digit);
+  }
+}
+
+let firstNum = localStorage.getItem("firstNum");
+let secNum = localStorage.getItem("secNum");
+let operator = localStorage.getItem("operator");
 
 let displayNum = ``;
 let displayScreen = ``;
+
+let displayVal;
 
 function displayValue(value) {
   displayNum = value;
@@ -41,29 +59,43 @@ numbers.forEach((e) => {
   e.addEventListener("click", () => {
     displayValue(e.innerHTML);
     console.log(displayNum);
-    if (firstNum < 1) {
-      firstNum = Number(e.innerHTML);
-      console.log(firstNum);
-    } else if (firstNum > 0) {
-      secNum = Number(e.innerHTML);
-      console.log(`sec : ${secNum}`);
-      firstNum = 0;
-    }
-
     displayScreen += displayNum;
+
     display.innerHTML = displayScreen;
+    displayVal = display.innerHTML;
+    if (!display.classList.contains("operated")) {
+      localStorage.setItem("firstNum", displayVal);
+    } else if (display.classList.contains("operated")) {
+      localStorage.setItem("secNum", displayVal);
+    }
   });
 });
 
 operators.forEach((e) => {
   e.addEventListener("click", () => {
+    operator = ``;
+
     if (operator === ``) {
+      localStorage.setItem("operator", e.innerHTML);
       displayOperator(e.innerHTML);
-      console.log(displayNum);
-      displayScreen += operator;
+      console.log(operator);
+      displayScreen = operator;
       display.innerHTML = displayScreen;
+
+      displayVal = display.innerHTML;
+      display.classList.add("operated");
     } else {
       operator = ``;
     }
   });
 });
+
+equal.addEventListener("click", () => {
+  console.log(operate("55", "66", "*"));
+});
+
+console.log(displayVal);
+console.log(firstNum);
+console.log(secNum);
+
+console.log(operator);
